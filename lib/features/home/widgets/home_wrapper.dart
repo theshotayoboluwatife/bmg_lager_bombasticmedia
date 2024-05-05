01dd23ef783a8a25931b79a3ad1dd60f457eb9f4
Shotayo Boluwatife <shotayobolu@gmail.com>
2024-05-05 16:04:02 +0100

    fixes in bottom nav bar

--- lib/features/home/widgets/home_wrapper.dart
+++ lib/features/home/widgets/home_wrapper.dart
@@ -34,8 +34,48 @@ class _HomeWrapperState extends State<HomeWrapper> {
         height: double.infinity,
         child: widget.navigationShell,
       ),
-      bottomNavigationBar: SlidingClippedNavBar(
-        backgroundColor: const Color(0xffEDEDED),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: selectedIndex,
+        items: <BottomNavigationBarItem>[
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag_rounded),
+            label: 'Products',
+          ),
+          const BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Image.asset(
+              'assets/images/scan_qr_icon.png',
+              width: 30,
+              height: 30,
+            ),
+            label: 'Scan QR',
+          ),
+        ],
+        backgroundColor: const Color(0xfff7f7f7),
+        iconSize: 30,
+        showSelectedLabels: true,
+        showUnselectedLabels: true,
+        unselectedItemColor: const Color(0xff6d6e76),
+        selectedItemColor: AppColor.blue,
+        selectedLabelStyle: TextStyle(
+            fontSize: 10, fontWeight: FontWeight.w500, color: AppColor.blue),
+        unselectedLabelStyle: const TextStyle(
+            fontSize: 10,
+            fontWeight: FontWeight.w300,
+            color: Color(0xff6d6e76)),
+        onTap: (index) {
+          setState(() {
+            selectedIndex = index;
+          });
+          _goBranch(selectedIndex);
+        },
+      ),
+
+      /* SlidingClippedNavBar(
+        backgroundColor: const Color(0xfff7f7f7),
         onButtonPressed: (index) {
           setState(() {
             selectedIndex = index;
@@ -43,24 +83,25 @@ class _HomeWrapperState extends State<HomeWrapper> {
           _goBranch(selectedIndex);
         },
         iconSize: 30,
-        inactiveColor: const Color(0xffADADAF),
+        inactiveColor: const Color(0xff6d6e76),
         activeColor: AppColor.blue,
         selectedIndex: selectedIndex,
+
         barItems: [
           BarItem(
-            icon: Icons.home,
-            title: 'Home',
+            icon: Icons.shopping_bag_rounded,
+            title: 'Products',
           ),
           BarItem(
-            icon: Icons.ios_share_outlined,
-            title: 'Export',
+            icon: Icons.search_sharp,
+            title: 'Search',
           ),
           BarItem(
             icon: Icons.qr_code_scanner_sharp,
             title: 'Scan',
           ),
         ],
-      ),
+      ),*/
     );
   }
 }

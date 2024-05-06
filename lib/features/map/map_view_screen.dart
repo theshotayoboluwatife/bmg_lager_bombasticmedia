108f7b93d3924e8d6046daf9353971c0ea44e8f8
Shotayo Boluwatife <shotayobolu@gmail.com>
2024-05-07 00:13:36 +0100

    map screen

--- lib/features/map/map_view_screen.dart
+++ lib/features/map/map_view_screen.dart
@@ -19,39 +19,41 @@ class MapViewScreen extends StatelessWidget {
               image: AssetImage('assets/images/maps.png'), fit: BoxFit.cover)),
       child: Padding(
         padding: const EdgeInsets.only(left: 16.0, right: 16.0, bottom: 30.0),
-        child: Align(
-          alignment: Alignment.bottomCenter,
-          child: Container(
-            padding: const EdgeInsets.all(12.0),
-            width: double.infinity,
-            height: 80.0,
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(16.0)
-            ),
-            child: Row(
-              children: [
-                const RoundedCornerImage(
-                    imageUrl: 'assets/images/bmg_tc2.png', imageSize: 50),
-                const Gap(10.0),
-                Column(
-                  children: [
-                    const AppText(
-                      text: 'NMK 316 Alexander street',
-                      fontSize: 16,
-                      fontWeight: FontWeight.w400,
-                    ),
-                    const Gap(8.0),
-                     AppText(
-                      text:
-                          'Longitude: 62°77’80.7”N    Latitude: 90°77’34”.96E',
-                      fontSize: 12,
-                      color: AppColor.grey,
-                      fontWeight: FontWeight.w400,
-                    ),
-                  ],
-                )
-              ],
+        child: SafeArea(
+          child: Align(
+            alignment: Alignment.bottomCenter,
+            child: Container(
+              padding: const EdgeInsets.all(12.0),
+              width: double.infinity,
+              height: 80.0,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16.0)
+              ),
+              child: Row(
+                children: [
+                  const RoundedCornerImage(
+                      imageUrl: 'assets/images/bmg_tc2.png', imageSize: 50),
+                  const Gap(10.0),
+                  Column(
+                    children: [
+                      const AppText(
+                        text: 'NMK 316 Alexander street',
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                      ),
+                      const Gap(8.0),
+                       AppText(
+                        text:
+                            'Longitude: 62°77’80.7”N    Latitude: 90°77’34”.96E',
+                        fontSize: 12,
+                        color: AppColor.grey,
+                        fontWeight: FontWeight.w400,
+                      ),
+                    ],
+                  )
+                ],
+              ),
             ),
           ),
         ),

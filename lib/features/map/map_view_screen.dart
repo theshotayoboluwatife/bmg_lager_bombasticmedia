48643f3be0c89336aafc9344caad6320c829458d
Shotayo Boluwatife <shotayobolu@gmail.com>
2024-05-08 20:51:54 +0100

    map is responsive

--- lib/features/map/map_view_screen.dart
+++ lib/features/map/map_view_screen.dart
@@ -29,46 +29,51 @@ class MapViewScreen extends StatelessWidget {
           ),
           Positioned(
             bottom: 0,
-            child: Container(
-              padding: const EdgeInsets.all(12.0),
-              width: double.infinity,
-              height: 120.0,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(16.0)),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const RoundedCornerImage(
-                      imageUrl: 'assets/images/bmg_trashcan.jpg',
-                      imageSize: 80),
-                  const Gap(10.0),
-                  Flexible(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Flexible(
-                          child: AppText(
-                            text: 'NMK 316 Alexander street',
-                            fontSize: 16,
-                            fontWeight: FontWeight.w400,
+            left: 0,
+            right:0,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom:20.0, left: 20.0, right:20.0),
+              child: Container(
+                padding: const EdgeInsets.all(12.0),
+                width: double.infinity,
+                height: 120.0,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(16.0)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const RoundedCornerImage(
+                        imageUrl: 'assets/images/bmg_trashcan.jpg',
+                        imageSize: 80),
+                    const Gap(10.0),
+                    Flexible(
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          const Flexible(
+                            child: AppText(
+                              text: 'NMK 316 Alexander street',
+                              fontSize: 16,
+                              fontWeight: FontWeight.w400,
+                            ),
                           ),
-                        ),
-                        const Gap(8.0),
-                        Flexible(
-                          child: AppText(
-                            text:
-                                'Longitude: 62°77’80.7”N    Latitude: 90°77’34”.96E',
-                            fontSize: 12,
-                            color: AppColor.grey,
-                            fontWeight: FontWeight.w400,
+                          const Gap(8.0),
+                          Flexible(
+                            child: AppText(
+                              text:
+                                  'Longitude: 62°77’80.7”N    Latitude: 90°77’34”.96E',
+                              fontSize: 12,
+                              color: AppColor.grey,
+                              fontWeight: FontWeight.w400,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ),
